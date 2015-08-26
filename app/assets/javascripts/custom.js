89efff8c849ea9df1183f5435efc1db06695afc9
Matthew Krog <mdkrog@gmail.com>
2015-08-26 13:02:31 +0200

    Add scroll effect to issue index

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,7 +1,23 @@
 $(function() {
   $('.cover-thumbnail').click(function(e){
     $(".cover-spread").attr('src',$(this).data("image-src"));
+    $(".featured-cover-link").attr('href', '/issues/' + $(this).data("issue-number"));
     $(".featured-issue-button-link").attr('href', '/issues/' + $(this).data("issue-number"));
     $(".featured-issue-button-link .btn-text").html('Read Issue ' + $(this).data("issue-number"));
+  });
+});
+
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
   });
 });
